9946817efeade37afe2ee6bbde2f7fd384231984
Gabor Claussnitzer <gabor.claussnitzer@googlemail.com>
2025-05-25 12:53:06 +0200

    wip

---
+++ .github/workflows/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+  echo "arg #1 must be a release tag name" && exit 1
+fi
+
+RELEASE_TAG_NAME="$1"
+
+# download openapi.yaml
+curl -L \
+-o openapi.yaml \
+https://github.com/gclaussn/go-bpmn/releases/download/${RELEASE_TAG_NAME}/go-bpmn-openapi.yaml
+
+# build OpenAPI documentation
+npx @redocly/cli build-docs \
+--title "go-bpmn HTTP API (${RELEASE_TAG_NAME})" \
+--output openapi.html \
+openapi.yaml
+
+# build documentation
+npm run build
+mv openapi.html ./dist/
